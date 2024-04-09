61c485a4698e114e2af48c7a0112c4c061d77a29
pc <kapellwork@gmail.com>
2024-04-09 15:12:46 +0300

    some fixes / pc init

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { ApiOperation } from '@nestjs/swagger';
+import { UserLoginDto } from './dto/user-login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -8,13 +9,17 @@ export class AuthController {
   @Post('/register')
   async register(@Body() userRegisterDto: UserRegisterDto) {
     if (userRegisterDto) {
-      return 'ok';
+      return 'register ok';
     }
   }
 
   @ApiOperation({ summary: 'User authorization' })
   @Post('/login')
-  async login() {}
+  async login(@Body() userLoginDto: UserLoginDto) {
+    if (userLoginDto) {
+      return 'login ok';
+    }
+  }
 
   @ApiOperation({ summary: 'Logging out of the system' })
   @Post('/logout')
