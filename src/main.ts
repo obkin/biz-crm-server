53767f3c6e39f95b5d7b76f5193938036ea928f8
obkin <kapellwork@gmail.com>
2024-04-09 12:17:02 +0300

    added: Swagger config (main.ts), Swagger connected to the app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
@@ -8,6 +9,22 @@ async function bootstrap() {
   app.useGlobalPipes();
   app.enableCors();
 
+  configureSwagger(app);
+
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
 bootstrap();
+
+function configureSwagger(app) {
+  const options = new DocumentBuilder()
+    .setTitle('BizCRM')
+    .setDescription(
+      'API for BizCRM application that helps managing your business',
+    )
+    .setVersion('1.0.0')
+    .addTag('nestjs')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
