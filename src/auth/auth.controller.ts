a8dfea809fd6817e9774d72987741cffe71d26a7
obkin <kapellwork@gmail.com>
2024-04-12 11:43:48 +0300

    added: auth module (empty)

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  async register() {
+    try {
+      // ..
+    } catch (e) {
+      // ..
+    }
+  }
+
+  async login() {
+    try {
+      // ..
+    } catch (e) {
+      // ..
+    }
+  }
+
+  async logout() {
+    try {
+      // ..
+    } catch (e) {
+      // ..
+    }
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { AuthRepository } from './auth.repository';
+import { LoggerService } from 'logger/logger.service';
 
-@Module({})
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService, AuthRepository, LoggerService],
+})
 export class AuthModule {}
