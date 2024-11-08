c3eb6aebd4ae2bb7724dbf05d3968f418acf6192
obkin <kapellwork@gmail.com>
2024-11-08 12:03:10 +0200

    fixed: modules imports

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { ProductsRepository } from './products.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from './entities/product.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService, ProductsRepository],
-  imports: [],
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
   exports: [ProductsService, ProductsRepository],
 })
 export class ProductsModule {}
