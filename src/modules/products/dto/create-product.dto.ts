2314a62848712711c8841fcc58edbd32b131b6dc
obkin <kapellwork@gmail.com>
2024-11-14 12:38:09 +0200

    fixed: CreateProductDto

--- src/modules/products/dto/create-product.dto.ts
+++ src/modules/products/dto/create-product.dto.ts
@@ -1,23 +1,70 @@
-import { IsString, IsNumber, IsOptional } from 'class-validator';
+import {
+  IsString,
+  IsNumber,
+  IsOptional,
+  IsNotEmpty,
+  MaxLength,
+  IsPositive,
+} from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateProductDto {
+  @ApiProperty({
+    example: 'Baseus Desktop Lamp',
+    description: 'The name of the product',
+  })
   @IsString()
-  name: string;
+  @IsNotEmpty()
+  @MaxLength(70, { message: 'Name must not exceed 70 characters' })
+  readonly name: string;
 
+  @ApiPropertyOptional({
+    example: 'A stylish desktop lamp with adjustable brightness',
+    description: 'The description of the product',
+  })
   @IsOptional()
   @IsString()
-  description?: string;
+  @MaxLength(9000, { message: 'Description must not exceed 9000 characters' })
+  readonly description?: string;
 
+  @ApiProperty({
+    example: 749.25,
+    description: 'The price of the product',
+  })
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsNotEmpty()
+  @IsPositive()
+  readonly price: number;
+
+  @ApiProperty({
+    example: 10,
+    description: 'The quantity of the product in stock',
+  })
   @IsNumber()
-  price: number;
+  @IsNotEmpty()
+  readonly quantity: number;
 
-  @IsNumber()
-  quantity: number;
-
+  @ApiPropertyOptional({
+    example: 'https://aws-s3/image?fh43FAgJ343gfhGHJ11344',
+    description: 'The URL for the product image',
+  })
   @IsOptional()
   @IsString()
-  imageUrl?: string;
+  readonly imageUrl?: string;
 
+  @ApiPropertyOptional({
+    example: 1,
+    description: 'The ID of the folder where the product is categorized',
+  })
   @IsOptional()
-  folderId?: number;
+  @IsNumber()
+  readonly folderId?: number;
+
+  @ApiPropertyOptional({
+    example: 123,
+    description: 'The ID of the user who owns this product',
+  })
+  @IsOptional()
+  @IsNumber()
+  readonly userId?: number;
 }
